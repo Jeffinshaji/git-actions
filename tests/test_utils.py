9feada59513395f10b6594fb0a990974eb0dcd48
Jeffin Shaji <jeffinshaji1997@gmail>
2025-11-04 11:58:46 +0530

    src edited

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,8 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.utils import add_numbers
+
 
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
